9632187f3be727bcc9b3a820766bc0ffbee79bd5
GitHub Action <action@github.com>
2021-01-28 15:50:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/qintianchen/intellijpluginstemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.qintianchen.intellijpluginstemplate.services
+
+import com.github.qintianchen.intellijpluginstemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
